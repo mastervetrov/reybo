18207a7c4d4abe78ec78ae3c051e1f12f70afeae
Rumyancev <rumiwan1997@gmail.com>
2026-03-22 17:26:47 +0700

    Some edited

--- shop/src/main/java/reybo/shop/ShopApplication.java
+++ shop/src/main/java/reybo/shop/ShopApplication.java
@@ -7,7 +7,7 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableDiscoveryClient
-@EnableFeignClients(basePackages = "like_shop.frontend.client")
+@EnableFeignClients(basePackages = "reybo.shop.client")
 public class ShopApplication {
 
 	public static void main(String[] args) {
