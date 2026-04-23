62d2b388f87263784c3a8049e7d54d4fad8b02d6
Rumyancev <rumiwan1997@gmail.com>
2026-04-23 20:26:46 +0700

    Security Config edited

--- gateway/src/main/java/com/reybo/gateway/common/security/SecurityConfiguration.java
+++ gateway/src/main/java/com/reybo/gateway/common/security/SecurityConfiguration.java
@@ -6,7 +6,6 @@ import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.oauth2.client.web.HttpSessionOAuth2AuthorizationRequestRepository;
-import org.springframework.security.oauth2.client.web.OAuth2AuthorizationRequestRepository;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.SimpleUrlAuthenticationFailureHandler;
 
